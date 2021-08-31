148bcb22549646cf4f11bef608a4fb9e1e89daed
Nancy Zaky <nancyzki@yahoo.com>
2021-08-31 14:24:05 -0700

    commit tree 4cd533ba674b3dda61b59bdadc51ace4030258c8

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves arr# code an argument here
   # Your code here
+  arr.each_with_index do |dwarf, index|
+  puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr# code an argument here
   # Your code here
+  dwarfs= arr.map(&:capitalize).map do |person|
+     person << "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls arr# code an argument here
   # Your code here
+newarr = arr.filter do |word|
+  word.length > 4
+end
+  newarr.length>0
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+if arr.include?("cheddar")
+'cheddar'
+elsif arr.include?("gouda")
+  "gouda"
+  elsif arr.include?("camembert")
+    "camembert"
+  else
+    nil
+  end
 end
